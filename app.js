235198c91f3681c47c34236df77b34554ad4f42f
Elizabeth Keyes <liz4momentum@gmail.com>
2023-02-28 16:04:43 -0700

    Clue button works

--- app.js
+++ app.js
@@ -3,52 +3,60 @@ const packages = [{
   isFragile: false,
   weight: 2,
   to: 'Sir Harrington IV',
-  trackingNumber: '1324kjs'
+  trackingNumber: '1324kjs',
+  isMissing: false
 },
 {
   priorityLevel: 'standard',
   isFragile: true,
   weight: .5,
   to: 'Master Mercutio',
-  trackingNumber: '1325sdk'
+  trackingNumber: '1325sdk',
+  isMissing: false
 },
 {
   priorityLevel: 'free',
   isFragile: true,
   weight: .5,
   to: 'Mistress Ravenfeather',
-  trackingNumber: 'jffd147'
+  trackingNumber: 'jffd147',
+  isMissing: false
 },
 {
   priorityLevel: 'standard',
   isFragile: false,
   weight: 4,
   to: 'B. Robert Brown',
-  trackingNumber: 'acdc145'
+  trackingNumber: 'acdc145',
+  isMissing: false
 },
 {
   priorityLevel: 'express',
   isFragile: true,
   weight: 6,
   to: 'Chancellor Wallace',
-  trackingNumber: '4b2l0z'
+  trackingNumber: '4b2l0z',
+  isMissing: false
 },
 {
   priorityLevel: 'standard',
   isFragile: false,
   weight: 5,
   to: 'Sarah Sharm',
-  trackingNumber: '8081baz'
+  trackingNumber: '8081baz',
+  isMissing: false
 },
 {
   priorityLevel: 'free',
   isFragile: true,
   weight: 12,
   to: 'Tae Lien',
-  trackingNumber: 'suz2367'
+  trackingNumber: 'suz2367',
+  isMissing: false
 }]
 
 drawPackages(packages)
+losePackage()
 
 let template = ""
 
@@ -60,10 +68,38 @@ function drawPackages(packages) {
     <p>TO: ${p.to}<br>
       WEIGHT: ${p.weight}<br>
         PRIORITY: ${p.priorityLevel}<br>
-          FRAGILE: ${p.isFragile}</p>
+          FRAGILE: ${p.isFragile ? 'yes' : 'no'}</p>
       </div>`
   })
   packageElem.innerHTML = template
+}
+
+function losePackage() {
+  let randomIndex = Math.floor(Math.random() * packages.length)
+  let lostPackage = packages[randomIndex]
+  lostPackage.isMissing = true
+  console.log(lostPackage, "losePackage function");
+}
+
+function giveClue() {
+  let lostPackage = packages.find(p => p.isMissing == true)
+  console.log(lostPackage, "giveClue function");
+  let clues = ["priorityLevel", "isFragile", "weight"]
+  let randomIndex = Math.floor(Math.random() * clues.length)
+  let randomClue = clues[randomIndex]
+  let clueElem = document.getElementById("clues")
+  // clueElem.innerText = randomClue
+  switch (randomClue) {
+    case "priorityLevel":
+      clueElem.innerHTML = `<p>The package has ${lostPackage.priorityLevel} delivery.</p>`
+      break;
+    case "isFragile":
+      clueElem.innerHTML = `<p>The package ${lostPackage.isFragile ? 'is fragile' : 'is not fragile'}</p>`
+      break;
+    case "weight":
+      clueElem.innerHTML = `<p>The package ${lostPackage.weight >= 5 ? 'is heavy' : 'is not heavy'}</p>`
+      break;
+  }
 }
 
 function filterHeavy() {
